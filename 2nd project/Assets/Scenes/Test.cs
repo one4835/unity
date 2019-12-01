7a4ade7d78c29a736aea2a46062bb79bce6ded54
one4835 <57549928+one4835@users.noreply.github.com>
2019-12-01 16:45:56 +0900

    2019.12.01. work

--- 2nd project/Assets/Scenes/Test.cs
+++ 2nd project/Assets/Scenes/Test.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
+	//2.7.4 인수가 있는 매서드
+	void Callname(string name)
+	{
+		Debug.Log("Hello " + name);
+	}
     void Start()
     {
-        Debug.Log("hello world");
-    }
-
+		Callname("Tom");
+	}
     // Update is called once per frame
     void Update()
     {
