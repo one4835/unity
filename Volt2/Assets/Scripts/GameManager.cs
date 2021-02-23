0135e3ad5ab1e925c919a204eb2e7a8b57784023
one4835 <57549928+one4835@users.noreply.github.com>
2021-02-23 16:46:48 +0900

    2021.02.23.

--- Volt2/Assets/Scripts/GameManager.cs
+++ Volt2/Assets/Scripts/GameManager.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public int[] jellyGoldList;
+
     public Vector3[] pointList;
+
+    public RuntimeAnimatorController[] LevelAc;
+
+    public void ChangeAc(Animator anim, int level)
+    {
+        anim.runtimeAnimatorController = LevelAc[level - 1];
+    }
 }
