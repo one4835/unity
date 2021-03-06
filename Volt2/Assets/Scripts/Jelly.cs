1979a65fae505916fd5b81bb85d7afc29d2d4c05
one4835 <57549928+one4835@users.noreply.github.com>
2021-03-06 13:32:54 +0900

    2020.03.06.

---
+++ Volt2/Assets/Scripts/Jelly.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jelly
+{
+    public int ID;
+    public int Level;
+    public float Exp;
+
+    public Jelly(int Id, int Level, float Exp)
+    {
+        this.ID = ID;
+        this.Level = Level;
+        this.Exp = Exp;
+    }
+}
